1eac78fb96d9a7729e15690466f49b7f3d725934
Julian Schmuckli <info@jeddsan.net>
2016-07-06 13:30:28 +0200

    Update demo.php

--- v1/demo.php
+++ v1/demo.php
@@ -3,9 +3,10 @@ require("compiler.php");
 $code='
 var data = "";
  data = jks_gt_json("data.json");
+ echo data["glossary"]["title"]."<br>";
  echo jks_math_sum   (4,5)."<br>";
  echo jks_math_sqro   (9)."<br>";
- //echo Round                (9)."<br>";
+ //echo ROUND                (9)."<br>";
  //ceIl(3)."<br>";
  echo jks_math_pi_number ()."<br>";
  echo jks_math_random (1,3)."<br>";
@@ -13,7 +14,7 @@ var data = "";
  echo jks_string_sub(2,2,"Hallo")."<br>";
  echo jks_string_i_find("Google","okay google")."<br>";
  echo jks_time_datetext("d.m.Y")."<br>";
- echo jks_time_convert_st(jks_time_datetext("d.m.Y",jks_time_curtim()))."<br>";
+ echo jks_time_string_c(jks_time_datetext("d.m.Y",jks_time_curtim()))."<br>";
  var array1 = array(1,4,2);
  jks_arr_order( array1)."<br>";
  jks_arr_prn( array1);
@@ -21,6 +22,15 @@ var data = "";
  if(jks_bool_bool_is( test)){
 	 echo "test is a bool";
  }
+ echo jks_type_get( test);
+ var hallo = jks_string_spli("Hallo");
+ jks_arr_prn( hallo);
+ echo round(6);
+ echo jks_math_bin2dec(100);
+ jks_arr_prn(jks_arr_values( array1));
+ echo jks_vali_url("https://www.norelect.ch/");
+ echo jks_vali_mailing("info@jeddsan.net");
+ echo jks_vali_name("Julian Schmuckli");
 ';
 echo nl2br(htmlspecialchars($code))."<br><br>";
 $jks_code = parseJKS($code);
